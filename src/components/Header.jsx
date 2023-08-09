f11429b033d6c73dd1b22b966cdbe25719649e02
Paztek <mathbarbier07@gmail.com>
2023-08-09 14:26:53 +0200

    code source

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
         <div className='header'>
-        <NavLink to="/">
+        <Link className='imglink' to ='/'>
         <img className='header_img' src='https://pbs.twimg.com/profile_images/1625842138943524864/muFoN7SB_400x400.jpg' alt=''/>
-        </NavLink>
+        </Link>
         <p className='header_title'>Candidature pour la formation Junior Developer</p>
         </div>
     );
